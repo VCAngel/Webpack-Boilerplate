161935e7c2c18371d91bf9b9a3b535ce59927e2b
VCAngel <vargascasavantesangel.329@gmail.com>
2022-03-05 03:07:29 -0700

    Boilerplate structure v.1.00

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './ReportWebVitals';
+import './styles/app.scss';
+
+ReactDOM.render(
+    <React.StrictMode>
+        {/* Components in here! */}
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+reportWebVitals();
